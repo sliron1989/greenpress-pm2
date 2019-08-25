b7d5115046e0543b1bce7707ec4c316e410cd881
David Meir-Levy <davidmeirlevy@gmail.com>
2019-08-25 16:21:48 +0300

    init data from node instead of pm2

---
+++ init-data.js
@@ -0,0 +1,32 @@
+const {spawn, fork} = require('child_process');
+const apps = require('./apps');
+console.log("run mongo");
+const mongo = spawn('mongod');
+
+process.once('exit', () => mongo.kill());
+
+mongo.stdout.on('data', data => {
+	if (data.toString().includes('waiting for connections on port')) {
+		console.log('db is running, start populate data');
+		Promise.all([
+			runServiceInit('content', apps.content.env),
+			runServiceInit('auth', apps.auth.env)
+		]).then(() => {
+			console.log('init data completed successfully!');
+			process.exit(0);
+		})
+	}
+});
+
+function runServiceInit(service, env) {
+	console.log('run', service);
+	return new Promise(resolve => {
+		const f = fork('./' + service + '/helpers/init.js', null, {env});
+
+		f.on('close', () => {
+			console.log(service, 'close');
+			resolve();
+		});
+	});
+
+}
